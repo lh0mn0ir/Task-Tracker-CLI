50dddba759f5d09d7d77ef0a53f9f763835950f9
lhomnoir <lhomnoir@outlook.fr>
2026-01-18 11:21:08 +0000

    fontion update

--- models.py
+++ models.py
@@ -1,21 +1,22 @@
 import controllers
-
+from datetime import datetime
 # Object task
 
 DATA = controllers.load()
 
 class Task:
     def __init__(self,  description, status):
-        self.id = None
+        self.id = len(DATA)+1
         self.description = description
         self.status = status
-        self.createdAt = None
-        self.updateAt = None
+        self.createdAt = datetime.now().strftime("%d/%m/%Y, %H:%M")
+        self.updateAt = datetime.now().strftime("%d/%m/%Y, %H:%M")
 
     def __str__(self):
         return self.description
 
     def add_task(self):
+        global DATA
         task = {"id": self.id,
                 "description": self.description,
                 "status": self.status,
@@ -27,8 +28,21 @@ class Task:
 
     @staticmethod
     def update_task(id_task:int, data:dict):
-        task = controllers.get_task(id_task=id_task)
-        task["description"] = data["description"] or task["description"]
-        task["status"] = data["status"] or task["status"]
-        task["updatedAt"] = data["updatedAt"]
-        controllers.save(task)
+            # Charger les données actuelles
+            for idx, task in enumerate(DATA):
+                if task["id"] == id_task:
+                    # Mettre à jour les champs si présents dans data
+                    task["description"] = data.get("description", task["description"])
+                    task["status"] = data.get("status", task["status"])
+                    task["updatedAt"] = datetime.now().strftime("%d/%m/%Y, %H:%M")
+                    DATA[idx] = task
+                    controllers.save(DATA)
+                    print("Sauvegarde éffectuée")
+                    return True
+            return False
+
+    @staticmethod
+    def delete_task(id_task):
+        for idx, task in enumerate(DATA):
+            if task["id"] == id_task:
+                DATA.pop(idx)
